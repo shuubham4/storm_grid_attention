d218e91cbf4bfdbe87f449d97fd2fe5e43a57120
shuubham4 <100393564+shuubham4@users.noreply.github.com>
2025-02-04 01:09:55 -0500

    Update unet_grid_attention_3D.py

--- sgmse/backbones/unet_grid_attention_3D.py
+++ sgmse/backbones/unet_grid_attention_3D.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 from .utils_grid_attention import UnetConv3, UnetUp3, UnetGridGatingSignal3
 import torch.nn.functional as F
-from models.layers.grid_attention_layer import GridAttentionBlock3D
-from models.networks_other import init_weights
+from .grid_attention_layer import GridAttentionBlock3D
+from .networks_other import init_weights
 
 class unet_grid_attention_3D(nn.Module):
 
